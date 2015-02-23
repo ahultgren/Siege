b427017f2d52e4d4d75dae6567470c588db427f1
Andreas Hultgren <me@andreashultgren.se>
2015-02-23 20:10:46 +0100

    Cleaned up some more usages of map.getAdjacent

--- app/shared/game/river.js
+++ app/shared/game/river.js
@@ -11,15 +11,15 @@ exports.connect = (map) => {
 
   tiles.forEach(tile => {
     if(tile.type === 'land') {
-      let adjacentTiles = map.getAdjacent(map, tile);
-      let adjacentRivers = adjacentTiles.filter(isRiver);
+      var adjacentTiles = tile.adjacent;
+      var adjacentRivers = adjacentTiles.filter(isRiver);
 
       // If and only if adjacent to two river tiles
       if(adjacentRivers.length === 2) {
         // They're not next to each other
         if(!map.isNextToEachOther(map, adjacentRivers)) {
           let adjacentAreBusy = adjacentRivers
-            .map((tile) => map.getAdjacent(map, tile).filter(isRiver).length >= 2)
+            .map((tile) => tile.adjacent.filter(isRiver).length >= 2)
             .some(Boolean);
 
           // Neither is already next to two rivers
@@ -33,7 +33,7 @@ exports.connect = (map) => {
 
   tiles.forEach(tile => {
     // Remove lonely river tiles
-    if(isRiver(tile) && !map.getAdjacent(map, tile).filter(isRiver).length) {
+    if(isRiver(tile) && !tile.adjacent.filter(isRiver).length) {
       tile.terrain = 'plains';
     }
   });
